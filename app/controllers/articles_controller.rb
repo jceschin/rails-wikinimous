2b90686dc5a28dd911d42f959cd770a347a4da0c
Juan Ceschin <juanceschin@gmail.com>
2020-06-13 18:47:40 -0300

    Add edit page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,18 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
+  # Update an article
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
   private
 
   def article_params
