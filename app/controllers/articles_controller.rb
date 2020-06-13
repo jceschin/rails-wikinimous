d35508a146094f0861587a7396364898dd3c33b8
Juan Ceschin <juanceschin@gmail.com>
2020-06-13 18:26:20 -0300

    Add new page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,22 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  # New instance variable
+  def new
+    @article = Article.new
+  end
+
+  # Create new article
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
